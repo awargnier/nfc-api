14e2335d299c2a7b508f1b4933d869e0a35aafd0
awargnier <adrienwarg@gmail.com>
2024-07-03 10:20:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from motor.motor_asyncio import AsyncIOMotorClient
 
-app = FastAPI(
-    docs_url='/'
-)
+app = FastAPI()
 
 # MongoDB connection
 client = AsyncIOMotorClient("mongodb+srv://allGroup:Estiam75020@nfcreader.5mrffd4.mongodb.net/")
